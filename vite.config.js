e6984cdbfebb2753349a9a8c88d2c99541a3290f
Hunain Parekh <hunain.parekh@xloopdigital.com>
2024-09-02 03:34:30 +0500

    Build Issue resolved

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',  // Set the root directory to src
-  publicDir: '../public',  // Reference the public directory correctly
   plugins: [react()],
 })
